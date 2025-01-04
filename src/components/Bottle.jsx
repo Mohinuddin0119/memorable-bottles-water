bbf0e0d56fab49fe8c1c1785847b9d1cb7cf7671
Mohin uddin <Mohinuddin0119@gmail.com>
2025-01-04 17:52:48 +0600

    added stock

--- src/components/Bottle.jsx
+++ src/components/Bottle.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './bottle.css'
 const Bottle = ({bottle,handleAddToCart}) => {
-    // console.log(bottle)
-    const {name,img,price,id} = bottle
+    console.log(bottle)
+    const {name,img,price,stock} = bottle
     return (
         <div className='bottle'>
             <img src={img} alt="" />
             <h3>Name: {name}</h3>
             <p>Price: ${price}</p>
-            <p>Id: {id}</p>
+            <p>Stock: {stock}</p>
             <button  onClick={()=>handleAddToCart(bottle)} style={{backgroundColor:'steelblue'}}>Purchase</button>
         </div>
     );
