6b4a2944161c02b1c75364ed748598c6ed4346f3
Mohin uddin <Mohinuddin0119@gmail.com>
2025-01-04 17:47:00 +0600

    added id

--- src/components/Bottle.jsx
+++ src/components/Bottle.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './bottle.css'
 const Bottle = ({bottle,handleAddToCart}) => {
     // console.log(bottle)
-    const {name,img,price} = bottle
+    const {name,img,price,id} = bottle
     return (
         <div className='bottle'>
             <img src={img} alt="" />
             <h3>Name: {name}</h3>
             <p>Price: ${price}</p>
+            <p>Id: {id}</p>
             <button  onClick={()=>handleAddToCart(bottle)} style={{backgroundColor:'steelblue'}}>Purchase</button>
         </div>
     );
